d31fbc912fa80a07c4d25835ef38527cab195377
leixiaochen <leixiaochen@agora.io>
2020-06-01 18:24:27 +0800

    made a change

--- day21-25/字典推导式.py
+++ day21-25/字典推导式.py
@@ -1,5 +1,6 @@
 DlAL_CODES = [(86, 'China'), (91, 'India'), (1, 'United Status'), (62, 'Indonesia'), (55, 'Brazil'), (81, 'Jpan')]
 country_code = {country: code for code, country in DlAL_CODES}
 print(country_code)
+# 大写字母并过滤小于60
 country_code2 = {code: country.upper() for country, code in country_code.items() if code < 60}
 print(country_code2)
